975c3972a2cf8cdb39b6c5770ef6a250bbced3d9
nandobfer <nandobfer@yahoo.com.br>
2023-11-14 14:11:03 -0300

    update: product list names

--- src/product_list.ts
+++ src/product_list.ts
@@ -1,7 +1,7 @@
 const featured: Product[] = [
     {
         id: 1,
-        name: "gel de limpeza renovador",
+        name: "gel de limpeza calmante",
         description:
             "Um gel facial que limpa profundamente sem ressecar, contribui para a prevenção de cravos e espinhas e controla a oleosidade da pele.",
     },
@@ -12,31 +12,31 @@ const featured: Product[] = [
     },
     {
         id: 3,
-        name: "sérum anti-idade",
+        name: "sérum calmante",
         description: "Sérum facial de rápida absorção que melhora e previne rugas finas hidrata, da brilho e firmeza à pele.",
         new: true,
     },
     {
         id: 4,
-        name: "tônico renovador",
+        name: "tônico nutritivo",
         description: "Tônico facial que renova diariamente a pele, esfoliando sem ressecar e deixando a pele luminosa, com viço e hidratada.",
         new: true,
     },
     {
         id: 5,
-        name: "esfoliante enzimático",
+        name: "esfoliante purificante antioleosidade",
         description: "Esfoliante enzimático que remove as células mortas sem agredir a pele, hidrata, repara e estimula a sua renovação.",
     },
     {
         id: 6,
-        name: "gel calmante",
+        name: "gel nutritivo hidratante",
         description:
             "Gel calmante que suaviza a pele, minimiza manchas causadas pelo sol, acne e Melasma, promovendo um tom mais uniforme e reduzindo a oleosidade.",
         new: true,
     },
     {
         id: 7,
-        name: "gel firmador antissinais",
+        name: "gel calmante",
         description: "Gel que melhora a firmeza da pele, suaviza linhas finas e hidrata profundamente por 48h.",
     },
     {
@@ -46,14 +46,18 @@ const featured: Product[] = [
             "Uma rotina completa para pele acneica. São 5 fórmulas para limpar sem ressecar, controlar a oleosidade, acalmar, tratar e prevenir a acne.",
         new: true,
     },
-    { id: 9, name: "kit hidratação, renovação e antienvelhecimento", description: "Géis para uma rotina de cuidados." },
+    { id: 9, name: "kit hidratação, antioleosidade e calmante", description: "Géis para uma rotina de cuidados." },
     {
         id: 10,
         name: "gel de limpeza antiacne",
         description: "Gel facial que remove todas as impurezas, contribui para prevenção de cravos e espinhas e controla a oleosidade da pele.",
         new: true,
     },
-    { id: 11, name: "tônico adstringente", description: "tônico facial que remove as impurezas da pele, deixa a pele luminosa, com viço e hidrata." },
+    {
+        id: 11,
+        name: "tônico matificante antiacne",
+        description: "tônico facial que remove as impurezas da pele, deixa a pele luminosa, com viço e hidrata.",
+    },
     {
         id: 12,
         name: "gel de limpeza hidratante",
@@ -68,7 +72,7 @@ const kits = [
         name: "kit limpeza, hidratação e proteção",
         description: "Inclui o limpador facial antiacne, hidratante e protetor solar FPS 60 garantindo uma pele limpa, protegida e hidratada.",
     },
-    { id: 14, name: "kit hidratação, renovação e antienvelhecimento", description: "Géis para uma rotina de cuidados essenciais." },
+    { id: 14, name: "kit hidratação, antioleosidade e calmante", description: "Géis para uma rotina de cuidados essenciais." },
     {
         id: 15,
         name: "kit tudinho",
@@ -81,13 +85,13 @@ const kits = [
 const cleaners = [
     {
         id: 16,
-        name: "gel de limpeza renovador",
+        name: "gel de limpeza hidratante",
         description: "Um gel facial que limpa profundamente sem ressecar, ilumina e clareia manhas da pele.",
     },
-    { id: 17, name: "gel de limpeza clareador", description: "Gel clareador que uniformiza a pele, removendo manchas causadas pelas acnes." },
+    { id: 17, name: "gel de limpeza calmante", description: "Gel clareador que uniformiza a pele, removendo manchas causadas pelas acnes." },
     {
         id: 18,
-        name: "gel de limpeza antiacne",
+        name: "gel de limpeza clareador antiacne",
         description: "Gel facial ideal para peles acneicas, reduzindo a oleosidade da pele sem deixar ressecada.",
     },
 ]
@@ -99,22 +103,26 @@ const oiled_skin = [
         description: "Gel facial ideal para peles acneicas, reduzindo a oleosidade da pele sem deixar ressecada.",
         new: true,
     },
-    { id: 20, name: "tônico adstringente", description: "tônico facial que remove as impurezas da pele, deixa a pele luminosa, com viço e hidrata." },
+    {
+        id: 20,
+        name: "tônico matificante antiacne",
+        description: "tônico facial que remove as impurezas da pele, deixa a pele luminosa, com viço e hidrata.",
+    },
     {
         id: 21,
-        name: "sérum retinol",
+        name: "sérum antioleosidade",
         description:
             "Sérum facial de rápida absorção que reduz de linhas e rugas, além da uniformização de textura e tom, luminosidade, aumento da firmeza e redução de poros.",
         new: true,
     },
     {
         id: 22,
-        name: "Esfoliante antiacne",
+        name: "Esfoliante purificante antioleosidade",
         description:
             "Remove células mortas, prevenindo cravos e espinhas, reduz a oleosidade da pele e desobstrui os poros. Enriquecido com ácido salicílico e ácido glicólico.",
         new: true,
     },
-    { id: 23, name: "creme facial", description: "Creme com toque seco que hidrata a pele por 48h, sem deixar oleosa." },
+    { id: 23, name: "creme facial hidratante antioleosidade", description: "Creme com toque seco que hidrata a pele por 48h, sem deixar oleosa." },
 ]
 
 export default { featured, cleaners, kits, oiled_skin }
